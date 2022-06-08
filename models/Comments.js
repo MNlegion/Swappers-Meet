8a399f121afa8e3e22a0025a33371f5a28f90cb3
MNlegion <rkreuser30@gmail.com>
2022-06-07 20:29:03 -0500

    comments model setup

--- models/Comments.js
+++ models/Comments.js
@@ -1,5 +1,30 @@
+// import important parts of sequelize library
 const { Model, DataTypes } = require('sequelize');
 
+// import our database connection from config.js
 const sequelize = require('../config/connection.js');
 
+// Initialize Comments model (table) by extending off Sequelize's Model class
 class Comments extends Model {}
+
+// set up fields and rules for Comments model
+// Need to continue to develop what this comments model entails
+Comments.init(
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+      }
+    },
+    {
+      sequelize,
+      timestamps: false,
+      freezeTableName: true,
+      underscored: true,
+      modelName: 'comments',
+    }
+  );
+  
+  module.exports = Comments;
