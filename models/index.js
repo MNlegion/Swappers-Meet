37913940d8f2d4726c885ee24d6e01ca3b012e79
MNlegion <rkreuser30@gmail.com>
2022-06-07 20:16:14 -0500

    initial model setup and config/connection file

---
+++ models/index.js
@@ -0,0 +1,10 @@
+// import all models
+const Product = require('./Product');
+const Category = require('./Category');
+const User = require('./User');
+const Comments = require('./Comments');
+
+// model associations
+
+
+// model exports
