1a9725a09cef28de260f27cd2473b18b95156ef9
Kayla Vangel <Kayla.Vangel@comcast.net>
2022-06-08 16:40:01 -0400

    added route files

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,17 +1,3 @@
 const router = require('express').Router();
 const { User, Product, Comments, Category} = require('../../models');
 
-router.get('/', (req, res) => {
-
-    // Posts FindAll limit 5
-    // .then dbposts data set to variable
-
-    res.render('homepage', {
-        dbposts,
-        loggedIn: req.session.loggedIn
-    })
-
-
-
-
-})
