8281b862810d4bc713226dc0d1650d32f5d0d8da
Kayla Vangel <Kayla.Vangel@comcast.net>
2022-06-10 13:25:44 -0400

    typo in comment model error

--- models/Comment.js
+++ models/Comment.js
@@ -10,7 +10,7 @@ class Comment extends Model {}
 // set up fields and rules for Comments model
 // Need to continue to develop what this comments model entails/ comment_text/ is_bid boolean,
 //references/ belongs to users/ belongs to one item
-Comments.init(
+Comment.init(
     {
       id: {
         type: DataTypes.INTEGER,
