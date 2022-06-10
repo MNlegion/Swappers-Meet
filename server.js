faa14125c0408245071f0f3a66bb25ee2df06b2a
Chrisborer <chris.borer@gmail.com>
2022-06-09 19:54:04 -0500

    starting of main page

--- server.js
+++ server.js
@@ -1,21 +1,41 @@
+const path = require('path');
 const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const routes = require('./controllers');
+const helpers = require('./utils/helpers');
+require('dotenv').config();
+
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const sequelize = require('./config/connection');
+// Set up custom helpers
+const hbs = exphbs.create({ helpers });
+const sess = {
+    secret: 'Thisistopreventfromgettinghacked',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
 
-const routes = require('./controllers');
+app.use(session(sess));
 
-//middleware
+//  Express.js will use handlebars template engine
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// app.use(express.static('public'));  //joining files in plublic folder
+app.use(express.static(path.join(__dirname, 'public')));
 
-// turn on routes
 app.use(routes);
 
-// turn on connection to db and server
-sequelize.sync({ force: true }).then(() => {  //configuration parameter ({force: true}) means that the databases must sync with the model definitions and associations or they recreate!
-    app.listen(PORT, () => console.log(`Now listening on ${PORT}`));
-  });
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Now listening on', PORT));
+});
