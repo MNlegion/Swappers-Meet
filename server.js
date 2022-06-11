7bfb2d9d9abadb6abf98b94ba6f0cba70489335b
Emma Olson <emma.olson794@gmail.com>
2022-06-11 12:27:57 -0400

    dashboard is working locally // WILL WORK WHEN MAIN HANDLEBARS ARE MERGED FROM OTHER BRANCH

--- server.js
+++ server.js
@@ -3,10 +3,8 @@ const sequelize = require('./config/connection');
 const routes = require('./controllers');
 const path = require('path');
 
-
 // ---------- FOR TESTING ONLY API ROUTES ---------- //
 // const routes = require('./controllers/api');
-
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -20,6 +18,12 @@ app.use(express.static(path.join(__dirname, 'public')));  //joining files in plu
 app.use(routes);
 
 app.use(require('./controllers/'));
+
+// express handlebars
+const { engine } = require('express-handlebars');
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+
 
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {  //configuration parameter ({force: true}) means that the databases must sync with the model definitions and associations or they recreate!
