ae6fb0a0a7e36cfc0b8f7d6cf5df38fd50455142
Emma Olson <emma.olson794@gmail.com>
2022-06-11 12:46:29 -0400

    basic marketplace page rendering

--- controllers/client/index.js
+++ controllers/client/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 const dashboardRoutes = require('./dashboard-routes');
+const marketplaceRoutes = require('./marketplace-route');
 
 router.use('/dashboard', dashboardRoutes);
+router.use('/marketplace', marketplaceRoutes);
 
 module.exports = router;
 
