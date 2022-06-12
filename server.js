ae7f2b84682937103dc96575818b57a4be9e5a8d
Kayla Vangel <Kayla.Vangel@comcast.net>
2022-06-12 19:22:54 -0400

    login and logout route, with auth, session and bcrypt tested

--- server.js
+++ server.js
@@ -1,9 +1,27 @@
 const express = require('express');
 const sequelize = require('./config/connection');
 const routes = require('./controllers');
+const session = require('express-session');
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+//session
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+  secret: 'theuncrackableunbreakablemysteriouspasswordthatyouwillneverhack',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
+
 
 //middleware
 app.use(express.json());
