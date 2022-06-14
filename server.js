a813f64cdcc9434286930c205c48d9aeaeca5db8
AndreBlankholm <ablankholm@gmail.com>
2022-06-13 20:12:19 -0500

    secret

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const PORT = process.env.PORT || 3001;
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
-  secret: 'theuncrackableunbreakablemysteriouspasswordthatyouwillneverhack',
+  secret: 'SECRET',
   cookie: {},
   resave: false,
   saveUninitialized: true,
