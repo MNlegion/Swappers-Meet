bf19ca5197a246230cedef970fcf7c8e16c26e6e
Emma Olson <emma.olson794@gmail.com>
2022-06-13 22:03:19 -0400

    working on home page route

--- controllers/client/index.js
+++ controllers/client/index.js
@@ -2,14 +2,11 @@ const router = require('express').Router();
 const dashboardRoutes = require('./dashboard-routes');
 const marketplaceRoutes = require('./marketplace-route');
 const myProductRoutes = require('./userItems-route');
+const homepageRoutes = require('./home-page-route');
 
 router.use('/dashboard', dashboardRoutes);
 router.use('/marketplace', marketplaceRoutes);
 router.use('/myproducts', myProductRoutes);
+router.use('/', homepageRoutes);
 
 module.exports = router;
-
-
-
-
-// router.use('/', catRoutes);
