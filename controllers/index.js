0562f46b9e0018606a0ce41041056c7f9440a2e8
justincode98 <test>
2022-06-14 19:14:22 -0700

    added login logic
    added buttons in handlebars and relevant button listeners in logic
    
    further cleaned up code
    added cheese

--- controllers/index.js
+++ controllers/index.js
@@ -2,14 +2,16 @@ const router = require('express').Router();
 
 const apiRoutes = require('./api');
 const clientRoutes = require('./client');
-
+//const homepageRoutes = require('./client/home-page-route');
 // router.use('/', clientRoutes);
 router.use('/api', apiRoutes);
+console.log("cheese8index");
 router.use('/', clientRoutes);
+//router.use('/home-page', homepageRoutes);
 
 // catchall
 router.use((req, res) => {
-  res.send("<h1>Sorry, non-existent route!</h1>")
+  res.send("non-existent route!")
 });
 
 module.exports = router;
